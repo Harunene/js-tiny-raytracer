655af872dea6315d58b2071a0639952aa1b1b57e
harunene <taskinrits@gmail.com>
2015-05-13 03:31:47 +0900

    리팩토링  - 멀티스레드 기능 추가. 이를 위해 각 클래스들이 tmp로 들고있는 값들을 전부 반환해서 밖에서 처리하도록 변경  - 스레드 4개로 돌리니 실행시간 2.5초가 1.8초로 줄었다 (기대한것보단 별로... 병목이 있나?)
    TODO : 전역변수랑 index.html에 있는 잘잘한 함수들 싹다 빼서 Renderer 클래스 만들자 싱글턴으로...

--- object.js
+++ object.js
@@ -7,8 +7,6 @@ function Sphere(pt, radius, c) {
 
   this.p = pt.clone();
   this.radius = radius;
-  this.firstT = 0;
-  this.firstPoint = new Vector(0, 0, 0);
   this.color = c.clone();
 }
 Sphere.prototype = {
@@ -20,29 +18,31 @@ Sphere.prototype = {
     d = b*b-a*c;
     if (d>=0)
     {
-      this.firstT = (-b-Math.sqrt(d))/a;
-      this.firstPoint = ray.at(this.firstT);
-      if (this.firstT < 0)
-        return false;
-      return true;
+      firstT = (-b-Math.sqrt(d))/a;
+      if (firstT < 0)
+        return undefined;
+      return firstT;
     }
-    return false;
+    return undefined;
   },
-  getGradient : function() {
-    var tmpvec = this.firstPoint.minus(this.p);
-    var tmpvec2 = light.minus(this.firstPoint);
+  getColor : function(firstPoint) {
+    return this.color;
+  },
+  getGradient : function(firstPoint) {
+    var tmpvec = firstPoint.minus(this.p);
+    var tmpvec2 = light.minus(firstPoint);
     var tmptheta = tmpvec.dot(tmpvec2) / (tmpvec.norm() * tmpvec2.norm());
     tmptheta = tmptheta * 0.5 + 0.5;
     return tmptheta;
   },
-  getReflectVector : function(startPos) {
+  getReflectVector : function(startPos, firstPoint) {
     var inVec = startPos.minus();
-    var N = new Vector(this.p, this.firstPoint);
+    var N = new Vector(this.p, firstPoint);
     return new Vector(N.multi((N.dot(inVec))/(N.dot(N))*2), inVec);
   },
-  getHighLight : function(startpos) {
-    var reflect = this.getReflectVector(light);
-    var eyevec = new Vector(this.firstPoint, startpos);
+  getHighLight : function(startpos, firstPoint) {
+    var reflect = this.getReflectVector(light, firstPoint);
+    var eyevec = new Vector(firstPoint, startpos);
     var tmp = reflect.dot(eyevec)/(reflect.norm()*eyevec.norm());
     tmp = (tmp>0)?Math.pow(tmp,31):0;
     return (tmp>0.80)?1:tmp;
@@ -61,8 +61,6 @@ function Triangle(p, p1, p2, c, c1, c2) {
   this.u = this.vec2.cross(this.N).div(d);
   this.v = this.vec1.cross(this.N).div(-d);
 
-  this.firstT = 0;
-  this.firstPoint = new Vector(0, 0, 0);
   this.color = new Vector(0, 0, 0);
   this.c = c.clone();
   this.c1 = c1.clone();
@@ -70,26 +68,26 @@ function Triangle(p, p1, p2, c, c1, c2) {
 }
 Triangle.prototype = {
   checkIntersection : function (ray) {
-    this.firstT = -1;
     var d = this.N.dot(ray.V);
     if (d == 0) 
-      return false;
-    this.firstT = this.N.dot(this.p.minus(ray.O)) / this.N.dot(ray.V);
-    this.firstPoint = ray.O.plus(ray.V.multi(this.firstT));
-    
-    var p = this.firstPoint.minus(this.p);
+      return undefined;
+    var firstT = this.N.dot(this.p.minus(ray.O)) / this.N.dot(ray.V);
+    var firstPoint = ray.O.plus(ray.V.multi(firstT));
+    var p = firstPoint.minus(this.p);
     var u = this.u.dot(p);
     var v = this.v.dot(p);
     if ( (u < 0) || (v < 0) || (u + v > 1) )
-    {
-      this.firstT = -1;
-      return false;
-    }
-    this.color = this.c.multi(1-u-v).plus(this.c1.multi(u)).plus(this.c2.multi(v));
-    return true;
+      return undefined;
+    return firstT;
   },
-  getGradient : function() {
-    var tmpvec = light.minus(this.firstPoint);    
+  getColor : function(firstPoint) {
+    var p = firstPoint.minus(this.p);
+    var u = this.u.dot(p);
+    var v = this.v.dot(p);
+    return this.c.multi(1-u-v).plus(this.c1.multi(u)).plus(this.c2.multi(v));
+  },
+  getGradient : function(firstPoint) {
+    var tmpvec = light.minus(firstPoint);    
     var tmptheta = this.N.dot(tmpvec) / (this.N.norm() * tmpvec.norm());
     tmptheta = tmptheta * 0.5 + 0.5;
     return tmptheta;
@@ -98,9 +96,9 @@ Triangle.prototype = {
     var inVec = startPos.minus();
     return new Vector(this.N.multi((this.N.dot(inVec))/(this.N.dot(this.N))*2), inVec);
   },
-  getHighLight : function(startpos) {
+  getHighLight : function(startpos, firstPoint) {
     var reflect = this.getReflectVector(light);
-    var eyevec = new Vector(this.firstPoint, startpos);
+    var eyevec = new Vector(firstPoint, startpos);
     var tmp = reflect.dot(eyevec)/(reflect.norm()*eyevec.norm());
     return (tmp>0)?Math.pow(tmp,31):0;
   },
